f208762caf5e1874fe58542d9fbe07f736ea8a61
Ritika0070 <ritikadixit66@gmail.com>
2026-08-15 00:16:59 +0530

    Time: 1546 ms (52.76%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
--- select name from  SalesPerson join
--- (select sales_id from Orders
--- group by com_id
---   HAVING NOT (com_id = 1))f
--- on f.sales_id =SalesPerson.sales_id;
+
 SELECT name
 FROM SalesPerson
 WHERE sales_id NOT IN (
