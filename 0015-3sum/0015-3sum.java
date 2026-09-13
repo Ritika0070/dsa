62d33c9000a86db371780d259a5d6b4cfa421023
Ritika0070 <ritikadixit66@gmail.com>
2026-09-13 12:40:29 +0530

    Time: 44 ms (17%), Space: 60.6 MB (5.09%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,31 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+    public List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort(arr);
         List<List<Integer>>list=new ArrayList<>();
-        int n=nums.length;
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-        int left=i+1;
-        int right=n-1;
-        while(left<right){
-              int sum=nums[i]+nums[left]+nums[right];
-        if(sum==0){
-            list.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
-            left++;
-            right--;
-            while(left<right && nums[left]==nums[left-1]) left++;
-              while(left<right && nums[right]==nums[right+1]) right--;
+        for(int i=0;i<arr.length-2;i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;
+        int x=arr[i];
+        int l=i+1;
+        int r=arr.length-1;
+        while(l<r){
+            List<Integer>a=new ArrayList<>();
+            int sum=x+arr[l]+arr[r];
+            if(sum==0){
+                a.add(x);
+               a.add(arr[l]);
+                a.add(arr[r]);
+                l++;
+                r--;
+                  while(l<r && arr[l]==arr[l-1]) l++;
+              while(l<r && arr[r]==arr[r+1]) r--;
+               list.add(a);
+            }else if(sum>0){
+                r--;
+            }else{
+                l++;
+            }
         }
-        else if(sum>0){
-            right--;
-        }else{
-            left++;
         }
+        return list;
     }
-        }
-    return list;
-}
 }
